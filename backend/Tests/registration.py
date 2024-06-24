871d3d5c046c524a8ecbbc436b2e7f6816b2cc55
Roland(z5413935) <roland.viray@yahoo.com>
2024-06-25 00:32:46 +1000

    removed unwanted comments

--- backend/Tests/registration.py
+++ backend/Tests/registration.py
@@ -16,23 +16,16 @@ login_data = {
     "email": "user@example.com",
     "password": "password123"
 }
-# Endpoint path
-# endpoint = '/register'
 
-# Full URL
-# url = base_url + endpoint
 
-# Send POST request with JSON data
+# Register
 response = requests.post(f"{base_url}/register", json=registration_data)
 print(f"Response status code: {response.status_code}")
 print("Response JSON(Register):")
 print(response.json())
 
-
+# Login
 response = requests.post(f"{base_url}/login", json=login_data)
 print(f"Response status code: {response.status_code}")
 print("Response JSON(Login):")
 print(response.json())
-
-# except requests.exceptions.RequestException as e:
-    # print(f"An error occurred: {e}")
